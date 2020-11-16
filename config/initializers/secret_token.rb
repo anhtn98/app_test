35796cffc78a9e01aa584429b02ef0beb0b61cfc
anhtn <anhtn@zigexn.vn>
2020-11-16 02:17:53 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppTest::Application.config.secret_key_base = '56168cd00b46c312e61a0b683e198b2fedf9fc42f343f84994ca8ecb3d17bb66433aa5a3930c3e20a807f9724db7c132daabd13f0d97a1dbc5b57ade2ac58f26'
